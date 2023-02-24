5111ad0b41df439fb5e614f04ebf8f37f567f6c9
Yenthe666 <yenthevg@gmail.com>
2023-02-24 11:17:59 +0100

    [REF] project_stage_update: update project stage based on task stages

--- project_stage_update/models/project_task_type.py
+++ project_stage_update/models/project_task_type.py
@@ -10,3 +10,8 @@ class ProjectTaskType(models.Model):
         help="The project stage that you configure here will define to which project stage the project will move "
              "when the last task is moved out of this stage."
     )
+
+    is_finished = fields.Boolean(
+        string='Final stage of a task',
+        help='If this boolean is checked on we will exclude tasks in this stage as those tasks are finished.'
+    )
